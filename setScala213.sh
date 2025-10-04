29173dd79f487337a1ccd8bc1435e0df7f6aad97
Jakob Holesek <jholesek@gmail.com>
2025-10-04 09:28:56 +0100

    added env set scripts.

---
+++ setScala213.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/opt/homebrew/Cellar/openjdk/21.0.2
+export PATH=/opt/homebrew/Cellar/scala@2.13/2.13.16/bin:$PATH
+
---
+++ setScala3.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/opt/homebrew/Cellar/openjdk/21.0.2
+export PATH=/opt/homebrew/bin:$PATH
+
