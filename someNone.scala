5aae9092e52d285a821984333b25353231fd7a94
Jakob Holesek <jholesek@gmail.com>
2025-10-15 19:54:30 +0100

    someNone final.

--- someNone.scala
+++ someNone.scala
@@ -11,3 +11,16 @@ val b = makeInt("one")
 println("a = ",a)
 println("b = ",b)
 
+val c = makeInt("1") match {
+  case Some(i) => i
+  case None => "Fail."
+}
+
+val d = makeInt("one") match {
+  case Some(i) => i
+  case None => "Fail."
+}
+
+println("c = ", c)
+println("d = ", d)
+
