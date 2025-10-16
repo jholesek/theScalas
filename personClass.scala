632a55614142ca81e7816654fd59a4b63fae73f3
Jakob Holesek <jholesek@gmail.com>
2025-10-16 18:27:34 +0100

    Person class final

--- personClass.scala
+++ personClass.scala
@@ -2,3 +2,9 @@ class Person(var firstName: String, var lastName:String) {
   def printFullName() = println(s"$firstName $lastName")
 }
 
+val p=new Person("John", "Doe")
+println(p.firstName)
+p.lastName="Legend"
+
+p.printFullName()
+
