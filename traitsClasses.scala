841f767c1f8792fec239cd25b93ece7af85b04d6
Jakob Holesek <jholesek@gmail.com>
2025-10-16 18:21:24 +0100

    Cat class.

--- traitsClasses.scala
+++ traitsClasses.scala
@@ -16,6 +16,19 @@ class Dog(name: String) extends Speaker with TailWagger with Runner {
   def speak(): String = "Woof!"
 }
 
+class Cat(name: String) extends Speaker with TailWagger with Runner {
+  def speak(): String = "Meow!"
+  override def startRun(): Unit = println("Running? What a joke!")
+  override def stopRun(): Unit = println("Who's running?")
+}
+
 val d = new Dog("Rover")
 println(d.speak())
+d.startRun()
+d.stopRun()
+
+val c = new Cat("Morris")
+println(c.speak())
+c.startRun()
+c.stopRun()
 
