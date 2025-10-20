f7000f4a3846b1e5b93a3cffb59c38eab026cb3f
Jakob Holesek <jholesek@gmail.com>
2025-10-20 17:35:22 +0100

    Fixed fpDomain test.

--- fpDomainModeling.scala
+++ fpDomainModeling.scala
@@ -1,11 +1,14 @@
 import CrustSize._
 
+// requires CrustSize.scala to be compiled!
+
 val currentCrustSize = Small
 
-currentCrustSize match {
+// does NOT work: currentCrustSize match {
+(currentCrustSize: CrustSize) match {
   case Small => println("Small crust")
-//  case Medium => println("Medium crust")
-//  case Large => println("Large crust")
+  case Medium => println("Medium crust")
+  case Large => println("Large crust")
 }
 
 if (currentCrustSize == Small) println("Still small crust.")
