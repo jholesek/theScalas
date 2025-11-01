30ef72f71abd1f4841b2d3ca7785437e590cdf72
Jakob Holesek <jholesek@gmail.com>
2025-11-01 19:05:28 +0000

    Added protected method.

--- Animal.scala
+++ Animal.scala
@@ -1,10 +1,21 @@
 class Animal {
         private def breathe()=println("I'm breathing")
+        def walk() = {
+                breathe()
+                println("I'm walking.")
+        }
+        protected def speak()=println("Hello?")
+
 }
 
 class Cat extends Animal {
         //this will not compile
-        override def breathe() =  println("Yo, breathing")
+        override def speak() =  println("Meow")
 }
 
+val cat = new Cat
+cat.walk()
+cat.speak()
+// this won't compile, it's private
+// cat.breathe()
 
