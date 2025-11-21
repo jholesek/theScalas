c48cacd1b1ce7fbdc59cffe31c9c96c97992a2da
Jakob Holesek <jholesek@gmail.com>
2025-11-21 19:38:05 +0000

    Added vector to the file.

--- listExmpl.scala
+++ listExmpl.scala
@@ -15,3 +15,21 @@ println(x.take(1))
 
 x.take(5).foreach(println)
 
+// vectors
+val strings = Vector("one", "two")
+
+println(strings(1))
+
+println(names(1))
+
+val av = Vector(1, 2, 3)
+val bv = av :+ 4
+val cv = av ++ Vector(4, 5)
+
+println(av , bv , cv)
+
+val dv = 0 +: av
+val ev = Vector(-1, 0) ++: av
+
+println(dv, ev)
+
