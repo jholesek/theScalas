914074deff37927615004c5f3ac46476cd63dacf
Jakob Holesek <jholesek@gmail.com>
2026-01-22 20:28:04 +0000

    added myUser type

---
+++ fileDownMan/myUser.scala
@@ -0,0 +1,5 @@
+case class myUser(
+        name: String,
+        email: String
+)
+
--- fileDownMan/testFileDownMan.scala
+++ fileDownMan/testFileDownMan.scala
@@ -1,6 +1,11 @@
 import myFile._
+import myUser._
 
 val f = myFile("first File","/home")
 
 println(f)
+
+val u = myUser("Jakob","jh@krneki.com")
+
+println(u)
 
