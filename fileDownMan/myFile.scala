275e344a5b577f1142a0e7e062f7a951e752b9bb
Jakob Holesek <jholesek@gmail.com>
2026-01-28 18:52:18 +0000

    Added goups to files.

--- fileDownMan/myFile.scala
+++ fileDownMan/myFile.scala
@@ -1,5 +1,8 @@
+import myGroup._
+
 case class myFile (
         name: String,
-        folder: String
+        folder: String,
+        groups: Seq[myGroup]
 )
 
--- fileDownMan/testFileDownMan.scala
+++ fileDownMan/testFileDownMan.scala
@@ -1,11 +1,20 @@
 import myFile._
 import myUser._
+import myGroup._
 
-val f = myFile("first File","/home")
+val f = myFile("first File","/home",Seq(myGroup("Silver tier")))
 
 println(f)
 
 val u = myUser("Jakob","jh@krneki.com")
 
 println(u)
+
+val g = myGroup("Bronze tier")
+
+println(g)
+
+val f1 = myFile("second File","/home/second",Seq(g))
+
+println(f1)
 
